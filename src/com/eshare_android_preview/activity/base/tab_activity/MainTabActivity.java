9453248f7c63c3da870d81037672c733de29f0dc
fushang318 <fushang318@gmail.com>
2013-10-23 12:00:34 +0800

    重构 首页 tab

--- src/com/eshare_android_preview/activity/base/tab_activity/MainTabActivity.java
+++ src/com/eshare_android_preview/activity/base/tab_activity/MainTabActivity.java
@@ -1,28 +1,27 @@
 package com.eshare_android_preview.activity.base.tab_activity;
 
+import java.util.ArrayList;
+
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TextView;
 import com.eshare_android_preview.R;
 
 public class MainTabActivity extends TabActivity  implements OnClickListener {
 	
-	private TabHost tab_Host;
-	int current_tab_id;
+	private TabHost tab_host;
+	private ArrayList<Tab> tabs = new ArrayList<Tab>();
+	Tab current_tab;
 	
 	FrameLayout tab_home_btn, tab_qa_btn, tab_message_btn;
-	ImageView tab_home_btn_image,tab_qa_btn_image,tab_message_btn_image;
-	TextView tab_home_btn_text, tab_qa_btn_text, tab_message_btn_text;
 	
 	private Animation left_in, left_out;
 	private Animation right_in, right_out;
@@ -30,15 +29,10 @@ public class MainTabActivity extends TabActivity  implements OnClickListener {
 	public void onCreate(Bundle savedInstanceState) {  
         super.onCreate(savedInstanceState);  
         setContentView(R.layout.tab_main);  
-        
         prepare_view();
         init_tab_switch_anim();
+        
         init_tab_host();
-		
-		tab_home_btn_image.setSelected(true);
-		tab_home_btn_text.setSelected(true);
-		current_tab_id = R.id.tab_home_btn;
-		
 		test_msg_show_view();
     }
 	
@@ -53,18 +47,6 @@ public class MainTabActivity extends TabActivity  implements OnClickListener {
 		tab_home_btn = (FrameLayout)findViewById(R.id.tab_home_btn);
 		tab_qa_btn = (FrameLayout)findViewById(R.id.tab_qa_btn);
 		tab_message_btn = (FrameLayout)findViewById(R.id.tab_message_btn);
-		// 底部三个切换页签按钮的图片
-		tab_home_btn_image = (ImageView) findViewById(R.id.tab_home_btn_image);
-		tab_qa_btn_image = (ImageView) findViewById(R.id.tab_qa_btn_image);
-		tab_message_btn_image = (ImageView) findViewById(R.id.tab_message_btn_image);
-		// 底部三个切换页签按钮的标题
-		tab_home_btn_text = (TextView) findViewById(R.id.tab_home_btn_text);
-		tab_qa_btn_text = (TextView) findViewById(R.id.tab_qa_btn_text);
-		tab_message_btn_text = (TextView) findViewById(R.id.tab_message_btn_text);
-		// 给底部三个切换页签的按钮注册点击事件
-		findViewById(R.id.tab_home_btn).setOnClickListener(this);
-		findViewById(R.id.tab_qa_btn).setOnClickListener(this);
-		findViewById(R.id.tab_message_btn).setOnClickListener(this);
 	}
 	
 	private void init_tab_switch_anim() {
@@ -76,93 +58,84 @@ public class MainTabActivity extends TabActivity  implements OnClickListener {
 	}
 	
 	private void init_tab_host() {
-		tab_Host = getTabHost();
-		tab_Host.addTab(build_tab_spec(
-				getResources().getString(R.string.category_home),
+		tab_host = getTabHost();
+		tab_host.addTab(build_tab_spec(
 				R.string.category_home,
-				R.drawable.tab_home_btn_image_bg_normal, 
+				tab_home_btn,
 				new Intent(this, HomeActivity.class)));
 		
-		tab_Host.addTab(build_tab_spec(
-				getResources().getString(R.string.category_qa),
-				R.string.category_qa, 
-				R.drawable.tab_qa_btn_image_bg_normal, 
+		tab_host.addTab(build_tab_spec(
+				R.string.category_qa,
+				tab_qa_btn,
 				new Intent(this, QAActivity.class)));
 		
-		tab_Host.addTab(build_tab_spec(
-				getResources().getString(R.string.category_message),
-				R.string.category_message, 
-				R.drawable.tab_message_btn_image_bg_normal, 
+		tab_host.addTab(build_tab_spec(
+				R.string.category_message,
+				tab_message_btn,
 				new Intent(this, MessageActivity.class)));
+	
+		Tab first_tab = tabs.get(0);
+		first_tab.btn.setSelected(true);
+		current_tab = first_tab;
+
 	}
 	
-	private TabHost.TabSpec build_tab_spec(String tag, int resLabel, int resIcon,
-			final Intent content) {
-		return tab_Host
-				.newTabSpec(tag)
-				.setIndicator(getString(resLabel),
-						getResources().getDrawable(resIcon))
-				.setContent(content);
+	private TabHost.TabSpec build_tab_spec(int res_label, FrameLayout btn, Intent content) {
+		String tag_name = getString(res_label);
+		int size = tabs.size();
+		Tab tab = new Tab(tag_name, btn, size);
+		tabs.add(tab);
+		btn.setTag(tab);
+		// 给底部切换页签的按钮注册点击事件
+		btn.setOnClickListener(this);
+		return tab_host.newTabSpec(tag_name).setIndicator(tag_name).setContent(content);
 	}
 	
 	
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0 && current_tab_id != R.id.tab_home_btn) {
+		if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0 && current_tab.index != 0) {
 			tab_home_btn.performClick();
 			return true;
 		}
 		return super.onKeyDown(keyCode, event);
 	}
+	
 	@Override
 	public void onClick(View v) {
-		if (current_tab_id == v.getId()) {
+		Tab click_tab = (Tab)v.getTag();
+		if (current_tab.index == click_tab.index) {
 			return;
 		}
+		current_tab.btn.setSelected(false);
 		
-		tab_home_btn_image.setSelected(false);
-		tab_home_btn_text.setSelected(false);
-		
-		tab_qa_btn_text.setSelected(false);
-		tab_qa_btn_image.setSelected(false);
-		
-		tab_message_btn_text.setSelected(false);
-		tab_message_btn_image.setSelected(false);
-		
-		int checked_id = v.getId();
-		boolean swtich_to_right = current_tab_id < checked_id ? true : false;
-		
+		boolean swtich_to_right = current_tab.index < click_tab.index ? true : false;
+		Animation out_anim;
+		Animation in_anim;
 		if (swtich_to_right){
-			tab_Host.getCurrentView().startAnimation(left_out);
+			out_anim = left_out;
+			in_anim = right_in;
 		}else{
-			tab_Host.getCurrentView().startAnimation(right_out);
+			out_anim = right_out;
+			in_anim = left_in;
 		}
 		
-		switch (checked_id) {
-		case R.id.tab_home_btn:
-			tab_Host.setCurrentTabByTag(getResources().getString(R.string.category_home));
-			tab_home_btn_image.setSelected(true);
-			tab_home_btn_text.setSelected(true);
-			break;
-		case R.id.tab_qa_btn:
-			tab_Host.setCurrentTabByTag(getResources().getString(R.string.category_qa));
-			tab_qa_btn_text.setSelected(true);
-			tab_qa_btn_image.setSelected(true);
-			break;
-		case R.id.tab_message_btn:
-			tab_Host.setCurrentTabByTag(getResources().getString(R.string.category_message));
-			tab_message_btn_text.setSelected(true);
-			tab_message_btn_image.setSelected(true);
-			break;
-		default:
-			break;
-		}
+		tab_host.getCurrentView().startAnimation(out_anim);
+		tab_host.setCurrentTabByTag(click_tab.tag_name);
+		click_tab.btn.setSelected(true);
+		tab_host.getCurrentView().startAnimation(in_anim);
+		current_tab = click_tab;
+	}
+	
+	class Tab{
+		public String tag_name;
+		public FrameLayout btn;
+		public int index;
 
-		if (swtich_to_right){
-			tab_Host.getCurrentView().startAnimation(right_in);
-		}else{
-			tab_Host.getCurrentView().startAnimation(left_in);
+		public Tab(String tag_name, FrameLayout btn, int index) {
+			this.tag_name = tag_name;
+			this.btn = btn;
+			this.index = index;
 		}
-		current_tab_id = checked_id;
-	} 
+	}
 }
