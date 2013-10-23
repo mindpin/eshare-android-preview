dcd4f4019680bb2b46096435d8a2dc00884bd3aa
fushang318 <fushang318@gmail.com>
2013-10-23 17:50:38 +0800

    重构 home activity

--- src/com/eshare_android_preview/activity/base/tab_activity/HomeActivity.java
+++ src/com/eshare_android_preview/activity/base/tab_activity/HomeActivity.java
@@ -1,9 +1,6 @@
 package com.eshare_android_preview.activity.base.tab_activity;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import java.util.ArrayList;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -12,7 +9,6 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.eshare_android_preview.R;
 import com.eshare_android_preview.activity.base.groups.GroupActivity;
 import com.eshare_android_preview.activity.base.knowledge_net.KnowledgeNetCategoryActivity;
@@ -20,7 +16,6 @@ import com.eshare_android_preview.activity.base.notes.NotesActivity;
 import com.eshare_android_preview.activity.base.plans.PlansActivity;
 import com.eshare_android_preview.activity.base.questions.KnowledgeNetQuestionActivity;
 import com.eshare_android_preview.base.activity.EshareBaseActivity;
-import com.eshare_android_preview.base.data.GridViewData;
 import com.eshare_android_preview.widget.adapter.GridViewAdapter;
 
 public class HomeActivity extends EshareBaseActivity {
@@ -46,7 +41,26 @@ public class HomeActivity extends EshareBaseActivity {
 		user_name.setText("功夫小子");
 	}
 	private void load_grid_view_data(){
-		final List<Map<String, Object>> lists = GridViewData.get_tab_home_grid_view_data();
+		ArrayList<GridViewData> lists = new ArrayList<GridViewData>();
+        lists.add(
+            new GridViewData("计划", R.drawable.tab_qa_btn_image_bg_normal, PlansActivity.class)
+        );
+        lists.add(
+            new GridViewData("笔记", R.drawable.tab_find_frd_normal, NotesActivity.class)
+        );
+        lists.add(
+            new GridViewData("收藏", R.drawable.tab_message_btn_image_bg_selected, FavourateActivity.class)
+        );
+        lists.add(
+            new GridViewData("学习", R.drawable.tab_find_frd_normal, KnowledgeNetCategoryActivity.class)
+        );
+        lists.add(
+            new GridViewData("题目", R.drawable.tab_message_btn_image_bg_selected, KnowledgeNetQuestionActivity.class)
+        );
+        lists.add(
+            new GridViewData("分组", R.drawable.tab_home_btn_image_bg_normal, GroupActivity.class)
+        );
+
 		GridViewAdapter adapter = new GridViewAdapter(this);
 		adapter.add_items(lists);
 		grid_view.setAdapter(adapter);
@@ -57,38 +71,16 @@ public class HomeActivity extends EshareBaseActivity {
 				
 				TextView info_tv = (TextView)list_item.findViewById(R.id.info_tv);
 				@SuppressWarnings("unchecked")
-				Map<String, Object> item = (HashMap<String, Object>) info_tv.getTag(R.id.tag_note_uuid);
-				open_click_item_activity(item);
+                GridViewData item = (GridViewData) info_tv.getTag(R.id.tag_home_grid_view_data);
+                open_activity(item.activity);
 			}
 		});
 	}
-	private void open_click_item_activity(Map<String, Object> item){
-		if (item.get("text").equals("计划")) {
-			open_activity(PlansActivity.class);
-		}
-		if (item.get("text").equals("笔记")) {
-			open_activity(NotesActivity.class);
-		}
-		if (item.get("text").equals("题目")) {
-			open_activity(KnowledgeNetQuestionActivity.class);
-		}
-		if (item.get("text").equals("学习")) {
-			open_activity(KnowledgeNetCategoryActivity.class);
-		}
 
-		if (item.get("text").equals("收藏")) {
-			open_activity(FavourateActivity.class);
-		}
-
-        if (item.get("text").equals("分组")) {
-            open_activity(GroupActivity.class);
-        }
-
-	}
-	
 	public void on_click_hard_right(View view){
 		
 	}
+
 	@Override 
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if (keyCode == KeyEvent.KEYCODE_BACK) {
@@ -96,4 +88,16 @@ public class HomeActivity extends EshareBaseActivity {
 		}
 		return super.onKeyDown(keyCode, event);
 	}
+
+    public class GridViewData {
+        public Class activity;
+        public int img;
+        public String text;
+
+        public GridViewData(String text, int img, Class activity){
+            this.text = text;
+            this.img = img;
+            this.activity= activity;
+        }
+    }
 }
