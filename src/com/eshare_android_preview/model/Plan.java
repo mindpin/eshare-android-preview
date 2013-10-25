b41686496ca9da3d03529ffff025a2283ac7a48b
menxu <menxu_work@163.com>
2013-10-25 17:37:11 +0800

    update plan

--- src/com/eshare_android_preview/model/Plan.java
+++ src/com/eshare_android_preview/model/Plan.java
@@ -1,8 +1,14 @@
 package com.eshare_android_preview.model;
 
+import java.io.Serializable;
+
 
 // 计划
-public class Plan {
+public class Plan implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	public int id;
 	public String content;
 	public String checked;
