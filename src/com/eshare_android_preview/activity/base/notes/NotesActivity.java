788124a11286bff5ffc592c0e6beb4f0e98586a1
fushang318 <fushang318@gmail.com>
2013-10-25 16:59:58 +0800

    增加笔记和收藏列表为空的提示

--- src/com/eshare_android_preview/activity/base/notes/NotesActivity.java
+++ src/com/eshare_android_preview/activity/base/notes/NotesActivity.java
@@ -20,22 +20,29 @@ public class NotesActivity extends EshareBaseActivity{
 	ListView list_view;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
 		setContentView(R.layout.n_notes);
 		
 		load_list();
-		
 		hide_head_setting_button();
         set_head_text(R.string.note);
+
         super.onCreate(savedInstanceState);
 	}
 	private void load_list() {
-		list_view = (ListView)findViewById(R.id.list_view);
 		List<Notes> notes_list = HttpApi.get_notes_list();
+        if(notes_list.size() == 0){
+            process_when_note_list_is_empty();
+        }else{
+            build_note_list_adapter(notes_list);
+        }
+	}
+
+    private void build_note_list_adapter(List<Notes> notes_list) {
 		NotesAdapter adapter = new NotesAdapter(this);
 		adapter.add_items(notes_list);
+
+		list_view = (ListView)findViewById(R.id.list_view);
 		list_view.setAdapter(adapter);
-		
 		list_view.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> list_view, View list_item,int item_id, long position) {
@@ -49,5 +56,11 @@ public class NotesActivity extends EshareBaseActivity{
 		        startActivity(intent);
 			}
 		});
-	}
+
+    }
+
+    private void process_when_note_list_is_empty() {
+        View note_list_empty_tip_tv = findViewById(R.id.note_list_empty_tip_tv);
+        note_list_empty_tip_tv.setVisibility(View.VISIBLE);
+    }
 }
