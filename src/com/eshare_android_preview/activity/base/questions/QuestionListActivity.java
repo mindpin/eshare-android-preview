775e797428f9eb26432d03d0c49525f4c3fdb002
ben7th <ben7th@sina.com>
2013-11-05 13:42:23 +0800

    update ui

--- src/com/eshare_android_preview/activity/base/questions/QuestionListActivity.java
+++ src/com/eshare_android_preview/activity/base/questions/QuestionListActivity.java
@@ -1,19 +1,15 @@
 package com.eshare_android_preview.activity.base.questions;
 
-import java.util.List;
-import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.eshare_android_preview.R;
 import com.eshare_android_preview.base.activity.EshareBaseActivity;
-import com.eshare_android_preview.model.Question;
-import com.eshare_android_preview.widget.adapter.QuestionsAdapter;
+import com.eshare_android_preview.base.utils.ImageTools;
 
 public class QuestionListActivity extends EshareBaseActivity{
-	ListView list_view;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,29 +17,17 @@ public class QuestionListActivity extends EshareBaseActivity{
 		
 		set_head_text(R.string.questions_title);
         hide_head_setting_button();
-		load_list_view();
+        _set_icon_fonts();
+        hide_head_bottom_line();
         super.onCreate(savedInstanceState);
 	}
-	
-	private void load_list_view() {
-		list_view = (ListView)findViewById(R.id.list_view);
-		final List<Question> question_list = Question.all();
-		QuestionsAdapter adapter = new QuestionsAdapter(this);
-		adapter.add_items(question_list);
-		list_view.setAdapter(adapter);
-		adapter.notifyDataSetChanged();
-		
-		list_view.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> list_view, View list_item,int item_id, long position) {
-                Question item = (Question) list_item.getTag(R.id.adapter_item_tag);
 
-                Intent intent = new Intent(QuestionListActivity.this, QuestionShowActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putSerializable(QuestionShowActivity.ExtraKeys.QUESTION, item);
-                intent.putExtras(bundle);
-		        startActivity(intent);
-			}
-		});
-	}
+    private void _set_icon_fonts() {
+        set_fontawesome((TextView) findViewById(R.id.question_icon));
+
+        BitmapDrawable drawable = ImageTools.toRoundCorner(
+                (BitmapDrawable) getResources().getDrawable(R.drawable.lan_js), 100);
+        ImageView lan_icon_iv = (ImageView) findViewById(R.id.lan_icon);
+        lan_icon_iv.setBackgroundDrawable(drawable);
+    }
 }
