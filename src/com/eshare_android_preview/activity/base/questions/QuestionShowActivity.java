6872186d0abfdf4356f63303769260932f77b0c1
ben7th <ben7th@sina.com>
2013-11-06 01:53:05 +0800

    update ui

--- src/com/eshare_android_preview/activity/base/questions/QuestionShowActivity.java
+++ src/com/eshare_android_preview/activity/base/questions/QuestionShowActivity.java
@@ -1,83 +1,85 @@
 package com.eshare_android_preview.activity.base.questions;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
 import com.eshare_android_preview.R;
 import com.eshare_android_preview.activity.base.notes.AddNoteActivity;
 import com.eshare_android_preview.base.activity.EshareBaseActivity;
-import com.eshare_android_preview.base.utils.BaseUtils;
 import com.eshare_android_preview.base.utils.ImageTools;
 import com.eshare_android_preview.model.Question;
 import com.eshare_android_preview.model.QuestionChoice;
 import com.eshare_android_preview.model.TestResult;
 
-public class QuestionShowActivity extends EshareBaseActivity{
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionShowActivity extends EshareBaseActivity {
     public static class ExtraKeys {
         public static final String QUESTION = "question";
         public static final String TEST_RESULT = "test_result";
     }
 
     TestResult test_result;
-    TextView test_result_tv;
 
-	TextView question_kind_tv, question_title_tv;
-	LinearLayout choices_detail_ll, choices_symbol_ll;
-	Button add_favourite_btn;
-	Button cancel_favourite_btn;
-	
-	EditText answer_et;
-	Button submit_answer_btn;
-	List<QuestionChoice> select_choices = new ArrayList<QuestionChoice>();
-	Question question;
+    TextView question_kind_tv, question_title_tv;
+    LinearLayout choices_detail_ll;
+    Button add_favourite_btn;
+    Button cancel_favourite_btn;
 
-	@SuppressLint("WorldReadableFiles")
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		setContentView(R.layout.q_question_show);
+    Button submit_answer_btn;
+    List<QuestionChoice> select_choices = new ArrayList<QuestionChoice>();
+    Question question;
+
+    @SuppressLint("WorldReadableFiles")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.q_question_show);
         hide_head_setting_button();
 
         Bundle bundle = getIntent().getExtras();
-        question = (Question)bundle.getSerializable(ExtraKeys.QUESTION);
-        test_result = (TestResult)bundle.getSerializable(ExtraKeys.TEST_RESULT);
-        if(test_result == null){
+        question = (Question) bundle.getSerializable(ExtraKeys.QUESTION);
+        test_result = (TestResult) bundle.getSerializable(ExtraKeys.TEST_RESULT);
+        if (test_result == null) {
             test_result = new TestResult();
         }
 
-		init_ui();
+        init_ui();
 
         refresh_test_result();
         init_faved_button();
-		load_question_msg();
+        load_question_msg();
 
         _set_icon_fonts();
 
         super.onCreate(savedInstanceState);
-	}
+    }
 
     private void _set_icon_fonts() {
         BitmapDrawable drawable = ImageTools.toRoundCorner(
                 (BitmapDrawable) getResources().getDrawable(R.drawable.lan_js), 100);
         ImageView lan_icon_iv = (ImageView) findViewById(R.id.lan_icon);
         lan_icon_iv.setBackgroundDrawable(drawable);
+
+        set_fontawesome((TextView) findViewById(R.id.correct_icon));
+        set_fontawesome((TextView) findViewById(R.id.error_icon));
     }
 
     private void refresh_test_result() {
-        test_result_tv = (TextView)findViewById(R.id.test_result_tv);
-        test_result_tv.setText("正确:" + test_result.correct_count + " 错误:" + test_result.error_count);
+        ((TextView) findViewById(R.id.correct_count)).setText(test_result.correct_count + "");
+        ((TextView) findViewById(R.id.error_count)).setText(test_result.error_count + "");
     }
 
     private void init_faved_button() {
@@ -93,58 +95,56 @@ public class QuestionShowActivity extends EshareBaseActivity{
     }
 
     private void init_ui() {
-		question_kind_tv = (TextView)findViewById(R.id.question_kind);
-		question_title_tv = (TextView)findViewById(R.id.question_title);
-		
-		choices_detail_ll = (LinearLayout)findViewById(R.id.choices_detail_ll);
-		choices_symbol_ll = (LinearLayout)findViewById(R.id.choices_symbol_ll);
-		
-		answer_et = (EditText)findViewById(R.id.answer_et);
-        submit_answer_btn = (Button)findViewById(R.id.submit_answer_btn);
-	}
-	
-	private void load_question_msg() {
+        question_kind_tv = (TextView) findViewById(R.id.question_kind);
+        question_title_tv = (TextView) findViewById(R.id.question_title);
+        choices_detail_ll = (LinearLayout) findViewById(R.id.choices_detail_ll);
+        submit_answer_btn = (Button) findViewById(R.id.submit_answer_btn);
+
+        findViewById(R.id.answer_correct_tip_tv).setVisibility(View.GONE);
+        findViewById(R.id.answer_error_tip_tv).setVisibility(View.GONE);
+        findViewById(R.id.next_question_btn).setVisibility(View.GONE);
+    }
+
+    private void load_question_msg() {
         set_head_text("练习题");
-		question_kind_tv.setText(question.get_kind_str());
-		question_title_tv.setText(question.title);
-		
-		load_choice(choices_detail_ll,R.layout.q_question_choice_detail_item);
-		load_choice(choices_symbol_ll,R.layout.q_question_choice_symbol_item);
-	}
+        question_kind_tv.setText(question.get_kind_str());
+        question_title_tv.setText(question.title);
 
-	private void load_choice(LinearLayout view,int item_view_layout){
-		for (QuestionChoice choice : question.choices_list) {
-			LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-			RelativeLayout choice_item_view = (RelativeLayout) inflater.inflate(item_view_layout, null); 
-			Button choice_item_btn = (Button) choice_item_view.findViewById(R.id.choice_item_btn);
+        load_choice(choices_detail_ll, R.layout.q_question_choice_detail_item);
+    }
 
-            String btn_text;
-            if(item_view_layout == R.layout.q_question_choice_detail_item){
-                btn_text = choice.content;
-            }else{
-                btn_text = choice.sym;
-            }
+    // 加载并显示选项
+    private void load_choice(LinearLayout view, int item_view_layout) {
+        for (QuestionChoice choice : question.choices_list) {
+            LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+            RelativeLayout choice_item_view = (RelativeLayout) inflater.inflate(item_view_layout, null);
 
-            choice_item_btn.setText(btn_text);
-			view.addView(choice_item_view);
-			choice_item_btn.setOnClickListener(new ClickItemListener(choice, choice_item_btn));
-		}
-	}
-	
-	public void click_on_submit_answer_btn(View view){
+            RelativeLayout choice_item_btn = (RelativeLayout) choice_item_view.findViewById(R.id.choice_item_btn);
+            TextView choice_item_sym  = (TextView) choice_item_view.findViewById(R.id.choice_item_sym);
+            TextView choice_item_text = (TextView) choice_item_view.findViewById(R.id.choice_item_text);
+
+            choice_item_sym.setText(choice.sym);
+            choice_item_text.setText(choice.content);
+            choice_item_btn.setOnClickListener(new ClickItemListener(choice, choice_item_btn));
+
+            view.addView(choice_item_view);
+        }
+    }
+
+    public void click_on_submit_answer_btn(View view) {
         View tip_tv;
         boolean is_answer_correct = true;
-        if(is_answer_correct){
+        if (is_answer_correct) {
             tip_tv = findViewById(R.id.answer_correct_tip_tv);
             test_result.increase_correct_count();
-        }else{
+        } else {
             tip_tv = findViewById(R.id.answer_error_tip_tv);
             test_result.increase_error_count();
         }
         refresh_test_result();
         tip_tv.setVisibility(View.VISIBLE);
         submit_answer_btn.setVisibility(View.GONE);
-        Button next_question_btn = (Button)findViewById(R.id.next_question_btn);
+        Button next_question_btn = (Button) findViewById(R.id.next_question_btn);
         next_question_btn.setVisibility(View.VISIBLE);
         next_question_btn.setOnClickListener(new OnClickListener() {
             @Override
@@ -158,84 +158,104 @@ public class QuestionShowActivity extends EshareBaseActivity{
                 finish();
             }
         });
-	}
-	
-	class ClickItemListener implements OnClickListener{
+    }
+
+    class ClickItemListener implements OnClickListener {
         QuestionChoice choice;
         View view;
-		public ClickItemListener(QuestionChoice choice,View view){
-			this.choice = choice;
-			this.view = view;
-		}
-		@Override
-		public void onClick(View v) {
 
-			if (question.is_single_choice() || question.is_true_false()) {
+        public ClickItemListener(QuestionChoice choice, View view) {
+            this.choice = choice;
+            this.view = view;
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            if (question.is_single_choice() || question.is_true_false()) {
                 select_choices.clear();
                 select_choices.add(choice);
-			}
-			if (question.is_multiple_choice()) {
-                if(select_choices.indexOf(choice) != -1){
+            }
+            if (question.is_multiple_choice()) {
+                if (select_choices.indexOf(choice) != -1) {
                     select_choices.remove(choice);
-                }else{
+                } else {
                     select_choices.add(choice);
                 }
-			}
-
-			set_choice_item_background(choices_detail_ll);
-			set_choice_item_background(choices_symbol_ll);
-
-            String answer = "";
-            for(QuestionChoice sc : select_choices){
-                answer += sc.sym;
             }
-			answer_et.setText(answer);
-			submit_answer_btn.setClickable(select_choices.size() != 0);
-		}
-		
-	}
-	
-	private void set_choice_item_background(LinearLayout layout){
-		if (question.is_single_choice() || question.is_true_false()) {
-			for (int j = 0; j < layout.getChildCount(); j++) {
-				layout.getChildAt(j).findViewById(R.id.choice_item_btn).setBackgroundResource(R.color.choice_before);
-			}
-			layout.getChildAt(select_choices.get(0).index).findViewById(R.id.choice_item_btn).setBackgroundResource(R.color.choice_selected);
-		}
-		
-		if (question.is_multiple_choice()) {
-			for (int j = 0; j < layout.getChildCount(); j++) {
-				layout.getChildAt(j).findViewById(R.id.choice_item_btn).setBackgroundResource(R.color.choice_before);
-			}
 
-            for(QuestionChoice qc : select_choices){
-			    layout.getChildAt(qc.index).findViewById(R.id.choice_item_btn).setBackgroundResource(R.color.choice_selected);
+            set_choice_item_background(choices_detail_ll);
+
+            if (select_choices.size() == 0) {
+                submit_answer_btn.setClickable(false);
+                submit_answer_btn.setBackgroundResource(R.drawable.btn_c6699bd3b_circle_flat);
+                submit_answer_btn.setShadowLayer(0, 0, 0, Color.parseColor("#00000000"));
+            } else {
+                submit_answer_btn.setClickable(true);
+                submit_answer_btn.setBackgroundResource(R.drawable.btn_c99bd3b_circle_flat);
+                submit_answer_btn.setShadowLayer(1, 1, 1, Color.parseColor("#66000000"));
             }
-		}
-	}
-	
-	public void click_notes(View view){
-		Bundle bundle = new Bundle();
+        }
+
+    }
+
+    private void set_choice_item_background(LinearLayout layout) {
+
+        if (question.is_single_choice() || question.is_true_false()) {
+            _set_choices_color_before(layout);
+            _set_choice_color_after(layout.getChildAt(select_choices.get(0).index));
+        }
+
+        if (question.is_multiple_choice()) {
+            _set_choices_color_before(layout);
+
+            for (QuestionChoice qc : select_choices) {
+                _set_choice_color_after(layout.getChildAt(qc.index));
+            }
+        }
+    }
+
+    private void _set_choices_color_before(LinearLayout layout) {
+        for(int i = 0; i < layout.getChildCount(); i++) {
+            View view = layout.getChildAt(i);
+            view.findViewById(R.id.choice_item_bg).setBackgroundColor(Color.parseColor("#ffffff"));
+            view.findViewById(R.id.choice_item_bg0).setBackgroundColor(Color.parseColor("#aaaaaa"));
+            view.findViewById(R.id.choice_item_sym).setBackgroundResource(R.drawable.btn_c444_circle_flat);
+            ((TextView) view.findViewById(R.id.choice_item_text)).setTextColor(Color.parseColor("#444444"));
+            ((TextView) view.findViewById(R.id.choice_item_text)).setShadowLayer(0, 0, 0, Color.parseColor("#00000000"));
+        }
+    }
+
+    private void _set_choice_color_after(View view) {
+        view.findViewById(R.id.choice_item_bg).setBackgroundColor(Color.parseColor("#99BD3B"));
+        view.findViewById(R.id.choice_item_bg0).setBackgroundColor(Color.parseColor("#82AA2A"));
+        view.findViewById(R.id.choice_item_sym).setBackgroundResource(R.drawable.btn_c435816_circle_flat);
+        ((TextView) view.findViewById(R.id.choice_item_text)).setTextColor(Color.parseColor("#ffffff"));
+        ((TextView) view.findViewById(R.id.choice_item_text)).setShadowLayer(1, 1, 1, Color.parseColor("#66000000"));
+    }
+
+    public void click_notes(View view) {
+        Bundle bundle = new Bundle();
         bundle.putSerializable(AddNoteActivity.ExtraKeys.LEARNING_RESOURCE, question);
 
-        Intent intent = new Intent(QuestionShowActivity.this,AddNoteActivity.class);
+        Intent intent = new Intent(QuestionShowActivity.this, AddNoteActivity.class);
         intent.putExtras(bundle);
         startActivity(intent);
-	}
-	
-	@SuppressLint({ "WorldReadableFiles", "WorldWriteableFiles" })
-	public void add_favourite(View view) {
+    }
+
+    @SuppressLint({"WorldReadableFiles", "WorldWriteableFiles"})
+    public void add_favourite(View view) {
         question.add_to_fav();
 
-		add_favourite_btn.setVisibility(View.GONE);
-		cancel_favourite_btn.setVisibility(View.VISIBLE);
-	}
-	
-	@SuppressLint({ "WorldReadableFiles", "WorldWriteableFiles" })
-	public void cancel_favourite(View view) {
+        add_favourite_btn.setVisibility(View.GONE);
+        cancel_favourite_btn.setVisibility(View.VISIBLE);
+    }
+
+    @SuppressLint({"WorldReadableFiles", "WorldWriteableFiles"})
+    public void cancel_favourite(View view) {
         question.remove_from_fav();
 
-		add_favourite_btn.setVisibility(View.VISIBLE);
-		cancel_favourite_btn.setVisibility(View.GONE);
-	}
+        add_favourite_btn.setVisibility(View.VISIBLE);
+        cancel_favourite_btn.setVisibility(View.GONE);
+    }
 }
--- src/com/eshare_android_preview/model/parse/YAMLParse.java
+++ src/com/eshare_android_preview/model/parse/YAMLParse.java
@@ -5,57 +5,61 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
 import org.json.JSONObject;
 import org.yaml.snakeyaml.Yaml;
+
 import android.content.res.AssetManager;
+
 import com.eshare_android_preview.application.EshareApplication;
 import com.eshare_android_preview.model.Question;
 import com.eshare_android_preview.model.QuestionChoice;
 
 public class YAMLParse {
-	public static List<Question> parse_yaml(String yaml_path){
-		List<Question> list = new ArrayList<Question>();
-		try {
-			AssetManager asset = EshareApplication.context.getAssets();
-			InputStream inputStream = asset.open(yaml_path);
-			Yaml yaml = new Yaml();
-			ArrayList<?> object = (ArrayList<?>) yaml.load(inputStream);
-			@SuppressWarnings("unchecked")
-			ArrayList<HashMap<String, JSONObject>> hms = (ArrayList<HashMap<String, JSONObject>>) object.get(1);
-			for (int i = 0; i < hms.size(); i++) {
-				HashMap<String, JSONObject> hashMap = hms.get(i);
-				Object knowledge_node_id = hashMap.get("knowledge_node_id");
-				Object kind = hashMap.get("kind");
-				Object title = hashMap.get("title");
+    public static List<Question> parse_yaml(String yaml_path) {
+        List<Question> list = new ArrayList<Question>();
+        try {
+            AssetManager asset = EshareApplication.context.getAssets();
+            InputStream inputStream = asset.open(yaml_path);
+            Yaml yaml = new Yaml();
+            ArrayList<?> object = (ArrayList<?>) yaml.load(inputStream);
+            @SuppressWarnings("unchecked")
+            ArrayList<HashMap<String, JSONObject>> hms = (ArrayList<HashMap<String, JSONObject>>) object.get(1);
+            for (int i = 0; i < hms.size(); i++) {
+                HashMap<String, JSONObject> hashMap = hms.get(i);
+                Object knowledge_node_id = hashMap.get("knowledge_node_id");
+                Object kind = hashMap.get("kind");
+                Object title = hashMap.get("title");
 
                 String[] choices_sym = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P".split(",");
-				List<QuestionChoice> choices_list = new ArrayList<QuestionChoice>();
-				Object answer = "";
-				if (kind.equals(Question.Type.SINGLE_CHOICE) || kind.equals(Question.Type.MULTIPLE_CHOICE)) {
-					Object choices = hashMap.get("choices");
-					ArrayList<?> choices_list_hm = (ArrayList<?>) choices;
-					for (int j = 0; j < choices_list_hm.size(); j++) {
-                        QuestionChoice qc = new QuestionChoice(j,choices_sym[j], choices_sym[j] + " " +  choices_list_hm.get(j).toString());
-						choices_list.add(qc);
-					}
-					answer = hashMap.get("answer");
-				}
+                List<QuestionChoice> choices_list = new ArrayList<QuestionChoice>();
+                Object answer = "";
 
-				if (kind.equals(Question.Type.TRUE_FALSE)) {
-					answer = hashMap.get("answer");
+                if (kind.equals(Question.Type.SINGLE_CHOICE) || kind.equals(Question.Type.MULTIPLE_CHOICE)) {
+                    Object choices = hashMap.get("choices");
+                    ArrayList<?> choices_list_hm = (ArrayList<?>) choices;
+                    for (int j = 0; j < choices_list_hm.size(); j++) {
+                        QuestionChoice qc = new QuestionChoice(j, choices_sym[j], choices_list_hm.get(j).toString());
+                        choices_list.add(qc);
+                    }
+                    answer = hashMap.get("answer");
+                }
+
+                if (kind.equals(Question.Type.TRUE_FALSE)) {
+                    answer = hashMap.get("answer");
                     choices_list.add(new QuestionChoice(0, "T", "正确"));
-                    choices_list.add(new QuestionChoice(1, "F","错误"));
-				}
+                    choices_list.add(new QuestionChoice(1, "F", "错误"));
+                }
 
-				if (!kind.equals(Question.Type.CODE)) {
-					Question question = new Question(i,knowledge_node_id.toString(), kind.toString(), title.toString(), choices_list, answer.toString());
-					list.add(question);
-				}
-				
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
+                if (!kind.equals(Question.Type.CODE)) {
+                    Question question = new Question(i, knowledge_node_id.toString(), kind.toString(), title.toString(), choices_list, answer.toString());
+                    list.add(question);
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
