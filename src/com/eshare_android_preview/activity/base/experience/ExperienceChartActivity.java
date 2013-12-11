a32057cc243b66dfec47abae12c2d633533ef9fa
mykingla <kingla_pei@163.com>
2013-12-11 16:21:35 +0800

    Add experience_chart_view

--- src/com/eshare_android_preview/activity/base/experience/ExperienceChartActivity.java
+++ src/com/eshare_android_preview/activity/base/experience/ExperienceChartActivity.java
@@ -1,14 +1,38 @@
 package com.eshare_android_preview.activity.base.experience;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.eshare_android_preview.R;
+import com.eshare_android_preview.base.activity.EshareBaseActivity;
+import com.eshare_android_preview.base.view.experience_chart_view.ExperienceChartView;
 
 
-public class ExperienceChartActivity extends Activity {
+public class ExperienceChartActivity extends EshareBaseActivity {
+
     public void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.experience_chart);
+        LinearLayout buttonGraph = new LinearLayout(this);
+        buttonGraph.setOrientation(LinearLayout.VERTICAL);
+
+
+        LinearLayout buttons = new LinearLayout(this);
+        buttons.setOrientation(LinearLayout.HORIZONTAL);
+
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT); // Verbose!
+        buttonGraph.addView(buttons, lp);
+
+        float[] xvalues = new float[] { 0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f };
+        float[] yvalues = new float[] { 0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f };
+
+
+        ExperienceChartView graph = new ExperienceChartView(this, xvalues, yvalues, 1);
+
+        buttonGraph.addView(graph, lp);
+
+        setContentView(buttonGraph);
+
         super.onCreate(savedInstanceState);
+
     }
 }
