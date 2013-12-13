4c31373e2da1dccfd8c0b487c895f1324fb7cefa
menxu <menxu_work@163.com>
2013-12-13 17:10:36 +0800

    add add method experienceOFView

--- src/com/eshare_android_preview/activity/base/expericence_of/ExperienceOfActiviey.java
+++ src/com/eshare_android_preview/activity/base/expericence_of/ExperienceOfActiviey.java
@@ -2,10 +2,14 @@ package com.eshare_android_preview.activity.base.expericence_of;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.eshare_android_preview.R;
 import com.eshare_android_preview.base.activity.EshareBaseActivity;
+import com.eshare_android_preview.base.utils.BaseUtils;
 import com.eshare_android_preview.base.view.ExperienceOfView;
+import com.eshare_android_preview.model.elog.CurrentState;
+import com.eshare_android_preview.model.elog.ExperienceLog;
 /**
  * Created by menxu on 13-12-10.
  */
@@ -23,7 +27,20 @@ public class ExperienceOfActiviey extends EshareBaseActivity{
 		experience_of_view = (ExperienceOfView)findViewById(R.id.experience_of_view);
 	}
 	public void click_experience_of_animator(View view){
-		experience_of_view.set_rect_width_animate(2/3F, 500);
+		EditText exp_tv = (EditText)findViewById(R.id.exp_tv);
+		String e_s = exp_tv.getText().toString();
+		int num = 0;
+		if (!BaseUtils.is_str_blank(e_s)) {
+			num = Integer.parseInt(e_s);
+//			BaseUtils.toast(num + " : ");
+		}
+		CurrentState state = ExperienceLog.current_state();
+		Float level_up_exp_num = (float) state.level_up_exp_num;
+		Float f = (num/level_up_exp_num);
+		
+		experience_of_view.add(num);
+		
+//		experience_of_view.set_rect_width_animate(2/3F, 500);
 //		experience_of_view.set_left_text(left_text);
 //		experience_of_view.set_right_text(right_text);
 	}
--- src/com/eshare_android_preview/base/view/ExperienceOfView.java
+++ src/com/eshare_android_preview/base/view/ExperienceOfView.java
@@ -9,25 +9,33 @@ import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.eshare_android_preview.base.utils.BaseUtils;
+import com.eshare_android_preview.model.elog.CurrentState;
+import com.eshare_android_preview.model.elog.ExperienceLog;
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.animation.Animator.AnimatorListener;
 import com.nineoldandroids.animation.ValueAnimator;
 
 /**
  * Created by menxu on 13-12-10.
  */
 public class ExperienceOfView extends View{
+	private boolean init_view = true;
+
+	private int druation = 500;
+	
 	private int rect_color = Color.GREEN; // 背景矩形颜色
 	
-	private Float left = 20F;  // 矩形的 left
+	private Float left = 60F;  // 矩形的 left
 	private Float top = 20F;   // 矩形的 top
 	
-	private Float rect_width = 100F; // 矩形里面的初始长
+	private Float rect_width = 220F; // 矩形里面的初始长
 	
-	
-	private Float rect_width_fill;
-	private Float rect_height;	
+	private Float rect_width_fill = 220F;
+	private Float rect_height = 50F;	
 	
 	// 圆
-	private Float circle_radius = 35F; // 圆半径
+	private Float circle_radius = 30F; // 圆半径
 
 	private Float circle_left_cx; // 左圆 坐标
 	private Float circle_left_cy;
@@ -42,9 +50,8 @@ public class ExperienceOfView extends View{
 	private int line_stroke_width = 5;
 	private int line_color = Color.argb(100, 0, 0, 0);
 	
-	// text
-	private String left_text = "3";
-	private String right_text = "4";
+	// current_level
+	private int current_level = 3;
 	 
 	private int text_color = Color.BLACK;
 	private float text_size = 16.0F;
@@ -63,23 +70,36 @@ public class ExperienceOfView extends View{
 		super.onDraw(canvas);
 		init();
 
-		draw_background_rect(canvas);
+		
 		draw_left_circle(canvas);
 		draw_right_circle(canvas);
 		draw_text_left(canvas);
 		draw_text_right(canvas);
+		
+		draw_background_rect(canvas);
 	}
 	private void init() {
-		// rect
-		this.rect_width_fill = getWidth() - left;
-		this.rect_height = getHeight() - top;
-		
 		// circle
-		this.circle_left_cx =  circle_radius + left/2;
-		this.circle_left_cy =  getHeight()/2F;
+		this.circle_left_cx =  left;
+		this.circle_left_cy =  top + rect_height/2 - line_stroke_width*2;
 		
-		this.circle_right_cx = getWidth() - circle_left_cx;
+		this.circle_right_cx = circle_left_cx + rect_width_fill - circle_radius - circle_left_cx/2 ;
 		this.circle_right_cy = circle_left_cy;
+		
+		if(init_view){
+			this.init_view = false;
+			set_default_rect_width();
+		}
+	}
+	
+	private void set_default_rect_width() {
+		CurrentState state = ExperienceLog.current_state();
+		Float level_up_exp_num = (float) state.level_up_exp_num;
+		Float exp_num = (float) state.exp_num;
+		Float fen_rect_width = (exp_num/level_up_exp_num);
+		this.rect_width  = (rect_width_fill - left) * fen_rect_width + left;
+		
+		this.current_level = state.level;
 	}
 	
 	private void draw_background_rect(Canvas canvas){
@@ -89,8 +109,7 @@ public class ExperienceOfView extends View{
 		paint.setStyle(Paint.Style.STROKE);
 		paint.setStrokeWidth(line_stroke_width);
 		paint.setColor(line_color);
-		RectF rectF = new RectF(left, top-line_stroke_width, rect_width_fill, rect_height+line_stroke_width);
-		canvas.drawRoundRect(rectF,30,30, paint);
+		canvas.drawRect(left, top-line_stroke_width, rect_width_fill, rect_height+line_stroke_width, paint);
 		
 		paint.setStyle(Paint.Style.FILL);
 		paint.setColor(rect_color);
@@ -129,9 +148,9 @@ public class ExperienceOfView extends View{
 		paint.setTextSize(text_size);
 		
 		Rect rect = new Rect();
-		paint.getTextBounds(left_text, 0, 1, rect); 
+		paint.getTextBounds(current_level+"", 0, 1, rect); 
 		
-		canvas.drawText(left_text, circle_left_cx - rect.width()/2, circle_left_cy, paint);
+		canvas.drawText(current_level+"", circle_left_cx - rect.width()/2, circle_left_cy, paint);
 	}
 	
 	private void draw_text_right(Canvas canvas){
@@ -140,44 +159,63 @@ public class ExperienceOfView extends View{
 		paint.setTextSize(text_size);
 		
 		Rect rect = new Rect();
-		paint.getTextBounds(right_text, 0, 1, rect); 
+		paint.getTextBounds((current_level + 1)+"", 0, 1, rect); 
 		
-		canvas.drawText(right_text, circle_right_cx - rect.width()/2, circle_right_cy, paint);
+		canvas.drawText((current_level + 1)+"", circle_right_cx - rect.width()/2, circle_right_cy, paint);
 	}
-	
-	
-	
-	public void set_rect_width(Float value){
-        this.rect_width = value;
+
+	private void set_rect_width(Float rect_width){
+        this.rect_width  = rect_width;
         invalidate();
         requestLayout();
     }
-
+	
+	private float get_add_rect(int count){
+		CurrentState state = ExperienceLog.current_state();
+		Float level_up_exp_num = (float) state.level_up_exp_num;
+		Float fen_rect_width = (count/level_up_exp_num);
+		
+		float rect = (rect_width_fill - left) * fen_rect_width;
+		return rect + this.rect_width;
+	}
+	
     // 根据传入的半径值和动画时间来产生动画效果
-    public void set_rect_width_animate(Float fen_rect_width, int druation) {
-    	float rect_width = rect_width_fill * fen_rect_width;
-    	
+    public void add(int count) {
         Float old_rect_width = this.rect_width;
-        ValueAnimator animation = ValueAnimator.ofFloat(old_rect_width, rect_width);
+
+        final float add_to = get_add_rect(count);
+        ValueAnimator animation = ValueAnimator.ofFloat(old_rect_width, add_to);
         animation.setDuration(druation);
         animation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 Float value = (Float) animation.getAnimatedValue();
-                set_rect_width(value);
+                value = value >= rect_width_fill ? rect_width_fill:value;
+                set_rect_width(value); 
             }
         });
+        animation.addListener(new AnimatorListener() {
+			@Override
+			public void onAnimationStart(Animator arg0) {}
+			@Override
+			public void onAnimationRepeat(Animator arg0) {}
+			@Override
+			public void onAnimationEnd(Animator arg0) {
+				if (rect_width_fill <= add_to) {
+					float current_count = ((add_to-rect_width_fill)/rect_width_fill) * ExperienceLog.get_level_up_exp_num_by(current_level);
+					set_velel(current_level+1);
+					set_rect_width(left);
+					add((int) current_count);
+				}
+			}
+			@Override
+			public void onAnimationCancel(Animator arg0) {}
+		});
         animation.start();
     }
     
-    public void set_left_text(String left_text){
-        this.left_text = left_text;
-        invalidate();
-        requestLayout();
-    }
-    
-    public void set_right_text(String right_text){
-        this.right_text = right_text;
+    private void set_velel(int velel){
+        this.current_level = velel;
         invalidate();
         requestLayout();
     }
