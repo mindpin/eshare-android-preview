08f7d4139c9e4acd640676d50b08eb7fefca2719
ben7th <ben7th@sina.com>
2013-12-20 17:22:56 +0800

    update question show

--- src/com/eshare_android_preview/base/view/QuestionChoicesView.java
+++ src/com/eshare_android_preview/base/view/QuestionChoicesView.java
@@ -5,7 +5,6 @@ import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
