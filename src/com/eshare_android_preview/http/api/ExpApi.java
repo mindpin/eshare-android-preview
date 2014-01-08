d9b6dd63982958a38e41b8d957bc5009e4abdda1
menxu <menxu_work@163.com>
2014-01-08 12:49:11 +0800

    Exp Test

---
+++ src/com/eshare_android_preview/http/api/ExpApi.java
@@ -0,0 +1,25 @@
+package com.eshare_android_preview.http.api;
+
+import com.eshare_android_preview.base.http.EshareGetRequest;
+import com.eshare_android_preview.http.model.CurrentState;
+import com.google.gson.Gson;
+
+public class ExpApi {
+	public static final String 经验 = "/knowledge_nets/";
+	public static final String 获取 = "/exp_info";
+	
+	public static CurrentState exp_info(String course_or_id){
+		try {
+			return new EshareGetRequest<CurrentState>(经验 + course_or_id + 获取) {
+				@Override
+				public CurrentState on_success(String response_text)throws Exception {
+					Gson gson = new Gson();
+					return gson.fromJson(response_text, CurrentState.class);
+				}
+			}.go();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
