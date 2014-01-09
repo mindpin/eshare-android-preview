89b354c4004ba3953e706cd7a8c6bda664eed3c5
mykingla <kingla_pei@163.com>
2014-01-09 09:27:00 +0800

    Add user login before test

--- test_src/com/eshare_android_preview/test/model/knowledge/ExpSuccessTest.java
+++ test_src/com/eshare_android_preview/test/model/knowledge/ExpSuccessTest.java
@@ -4,6 +4,7 @@ package com.eshare_android_preview.test.model.knowledge;
 import android.test.AndroidTestCase;
 
 import com.eshare_android_preview.http.api.TestSuccessHttpApi;
+import com.eshare_android_preview.http.api.UserAuthHttpApi;
 import com.eshare_android_preview.http.model.TestSuccess;
 
 import junit.framework.Assert;
@@ -18,8 +19,10 @@ public class ExpSuccessTest extends AndroidTestCase {
 
     public void test_add_exp_num() {
         try {
+            UserAuthHttpApi.user_authenticate("admin@edu.dev", "1234");
+
             test_success = TestSuccessHttpApi.build_test_success("javascript", "node-31");
-            Assert.assertEquals(test_success.add_exp_num, 0);
+            Assert.assertEquals(test_success.add_exp_num, 5);
             Assert.assertEquals(test_success.history_info.length, 5);
             Assert.assertEquals(test_success.day_exp(0).week_day, "周三");
             Assert.assertEquals(test_success.day_exp(1).month_day, "7");
