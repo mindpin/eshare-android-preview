924c0d0c1467963678ac517adfdec0a5b12b6045
ben7th <ben7th@sina.com>
2014-01-17 18:16:27 +0800

    update animate

---
+++ src/com/eshare_android_preview/view/ui/animate/SimpleAnimatorListener.java
@@ -0,0 +1,28 @@
+package com.eshare_android_preview.view.ui.animate;
+
+import com.nineoldandroids.animation.Animator;
+
+/**
+ * Created by Administrator on 14-1-17.
+ */
+public class SimpleAnimatorListener implements Animator.AnimatorListener {
+    @Override
+    public void onAnimationStart(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animator) {
+
+    }
+}
